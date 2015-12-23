f908beed04abd8b3d9df6c284f18cc4c99d304a3
Wouter Dhaeze <wouter@custodix.com>
2015-12-23 17:56:16 +0100

    Adjust one-pager for mobile devices

--- webroot/js/cdo.js
+++ webroot/js/cdo.js
@@ -11,23 +11,30 @@
 		$scope.submitInterest = function() {
 			$scope.showInProgress = true;
 			
+			var modal = UIkit.modal("#mdlSaveEmail");
+			modal.show();
+			
 			var i = {};
 			i.email = this.email;
 			
 			var res = $http.post('/api/interest.json', angular.toJson(i));
 			res.success(function(data, status, headers, config) {
-				alert("save success: " + data);
+				//alert("save success: " + data);
 				//$scope.subscription = data;
 				//$scope.debugHelp = data;
+				$scope.showError = false;
 				$scope.showSuccess = true;
 				$scope.showInProgress = false;
+				modal.hide();
 			});
 			res.error(function(data, status, headers, config) {
-				alert("failure message: " + status);
+				//alert("failure message: " + status);
 				//alert( "failure message: " + JSON.stringify({data: data}));
 				$log.debug(data);
-				$scope.showError = false;
+				$scope.showError = true;
+				$scope.showSuccess = false;
 				$scope.showInProgress = false;
+				modal.hide();
 			});	
 			
 			$scope.email = '';
