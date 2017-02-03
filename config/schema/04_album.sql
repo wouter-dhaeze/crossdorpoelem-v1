242382e5dc0e24d4e0031a572d489556158c6d82
wouter-dhaeze <wouter.dhaeze@custodix.com>
2017-02-03 21:32:08 +0100

    Added DB schema for albums

--- config/schema/04_album.sql
+++ config/schema/04_album.sql
@@ -2,7 +2,9 @@ CREATE TABLE `album` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `created` datetime DEFAULT NULL,
   `name` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
   `path` varchar(255) NOT NULL,
+  `photo` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name_UNIQUE` (`name`)
 )
